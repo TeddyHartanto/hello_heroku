631d0a286dfcf92b1a46391d0ad1498c1f1fc92c
Teddy Hartanto <hartantoteddy@yahoo.com>
2017-02-07 11:15:17 +0800

    Added logging

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 import os
+import logging
 from flask import Flask
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def hello():
+    logging.debug('saying hello')
     return 'Hello Teddy!'
 
